c0883dc0e378e115601a430e2ac943db7e44b71e
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2024-02-11 12:04:54 -0800

    format level1 main.py

--- level1/main.py
+++ level1/main.py
@@ -1,9 +1,8 @@
-IN_FILE="data/in_data.csv"
-OUT_FILE="data/out_data.csv"
+IN_FILE = "data/in_data.csv"
+OUT_FILE = "data/out_data.csv"
 
 
-
-def read_csv(path:str) -> list[list[str]]:
+def read_csv(path: str) -> list[list[str]]:
     with open(path, "r") as f:
         contents = f.read()
 
@@ -11,25 +10,24 @@ def read_csv(path:str) -> list[list[str]]:
     for line in contents.split("\n"):
 
         # skip final line of input file if empty
-        if len(line) == 0: 
+        if len(line) == 0:
             continue
 
         line_list = []
         for val in line.split(","):
             line_list.append(val)
         out_list.append(line_list)
-        
-    return out_list
 
+    return out_list
 
 
 def write_csv(data_to_write: list[list[str]], path: str):
     # convert 2D list of strings to a single string joined by "," and "\n"
     #   the inner list comprehension iterates over each row,
     #   takes the line: list[str], and converts it into a single string. with join
-    #   the outer join takes the results list[str] where each string is was a row, 
+    #   the outer join takes the results list[str] where each string is was a row,
     #   and joins them all into single string
-        
+
     #   Note: list comprehension do not mutate the list they iterate over,
     #       they copy and tranfrom and produce a new list
     #
@@ -38,7 +36,7 @@ def write_csv(data_to_write: list[list[str]], path: str):
     #
     str_data = "\n".join([",".join(line) for line in data_to_write])
     with open(path, "w") as f:
-        f. write(str_data)
+        f.write(str_data)
 
 
 def transform():
@@ -47,10 +45,10 @@ def transform():
     out_data_headers = ["name", "can_drink", "date", "age"]
     out_data = [out_data_headers]
 
-     # [1:] to skip in_data's headers
+    # [1:] to skip in_data's headers
     for in_line in in_data[1:]:
 
-        # initialize list of empty strings, with length of out_data_headers 
+        # initialize list of empty strings, with length of out_data_headers
         #   this is a list comprehension
         #   list comprehensions save the common pattern of,
         #       - initialize empty list
@@ -58,18 +56,18 @@ def transform():
         #       - insert something into initialized empty list
         #       - I did this in read_csv, twice in a nested way
         #   _ means ignore this value
-        out_line = ["" for _ in range(len(out_data_headers))] 
-        
+        out_line = ["" for _ in range(len(out_data_headers))]
+
         # here is the copying and transforming
-        out_line[0] = "Mr. " + in_line[1] # make it fancy
-        out_line[1] = "yes" if int(in_line[2]) >= 21 else "no" # google "python inline ternary"
+        out_line[0] = "Mr. " + in_line[1]  # make it fancy
+        out_line[1] = (
+            "yes" if int(in_line[2]) >= 21 else "no"
+        )  # google "python inline ternary"
         out_line[2] = in_line[0]
         out_line[3] = in_line[2]
         out_data.append(out_line)
 
     write_csv(out_data, OUT_FILE)
-
-
 
 
 if __name__ == "__main__":
